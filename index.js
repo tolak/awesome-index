38978812a33b6f65cdfacf02f2aa7c4ff1cf61e2
tolak <kalot.wang@gmail.com>
2023-09-21 14:29:33 +0800

    update index sdk

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const ethers = require('ethers')
 const {ASSETS, Client, Environment} = require('@phala/index')
-const solution_Moonbeam_WGLMR_AstarEvm_GLMR = require('./Moonbeam_WGLMR_AstarEvm_GLMR.json')
+const solution_Moonbeam_GLMR_AstarEvm_GLMR = require('./Moonbeam_GLMR_AstarEvm_GLMR.json')
 
 async function main() {
     const wallet = new ethers.Wallet(
@@ -13,19 +13,22 @@ async function main() {
     const moonbeam = indexClient.createEvmChain('Moonbeam')
     const asset = ASSETS.Moonbeam.GLMR
     const recipient = '<your account address on dest chain>'
-    // 1 GLMR
-    const amount = '1000000000000000000'
+    // 0.5 GLMR
+    const amount = '500000000000000000'
     const deposit = await moonbeam.getDeposit(
         asset,
         amount,
         recipient,
         // Routing solution of swap GLMR on Moonbeam to GLMR on Astar EVM env
         // Moonbeam/GLMR -> Moonbeam/WGLMR -> Moonbeam/xcDOT -> Moonbeam/xcPHA -> Phala/PHA -> Astar/PHA -> AstarEvm/PHA -> AstarEvm/WASTR -> AstarEvm/GLMR
-        solution_Moonbeam_WGLMR_AstarEvm_GLMR,
+        solution_Moonbeam_GLMR_AstarEvm_GLMR,
     )
 
     const txResponse = await wallet.sendTransaction(deposit.tx)
     console.log(`Transaction has been sent: ${txResponse.hash}`)
+
+    // Query status of task
+    // const task = await indexClient.getTask(deposit.id)
 }
 
 main()
